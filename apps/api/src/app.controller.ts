dd377a4287785e464f05b47913dffd1f9c4d4227
Vladyslav Panasyuk <panasyuk.vladyslav.kpi@gmail.com>
2026-04-25 09:53:46 +0300

    feat: add communication to api with Casdoor

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,12 +1,62 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  InternalServerErrorException,
+  Query,
+  Req,
+  Res,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
+import express from 'express';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private readonly webUrl: string;
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly appService: AppService,
+  ) {
+    this.webUrl = this.configService.getOrThrow<string>('WEB_URL');
+  }
+
+  @Get('login')
+  login(@Res() res: express.Response) {
+    const authUrl = this.appService.getAuthorizationUrl();
+    res.redirect(authUrl);
+  }
+
+  @Get('callback')
+  async callback(@Query('code') code: string, @Res() res: express.Response) {
+    try {
+      const tokenResponse = await this.appService.exchangeCodeForToken(code);
+
+      res.cookie('access_token', tokenResponse.access_token, {
+        httpOnly: true,
+        sameSite: 'lax',
+      });
+
+      res.redirect(this.webUrl);
+    } catch {
+      throw new InternalServerErrorException('Authorization error');
+    }
+  }
+
+  @Get('user-info')
+  async getUserInfo(@Req() req: express.Request) {
+    const token = req.cookies['access_token'] as string;
+
+    if (!token) {
+      throw new UnauthorizedException('Token missing');
+    }
+
+    try {
+      const userInfo = await this.appService.getUserProfile(token);
+      return userInfo;
+    } catch {
+      throw new UnauthorizedException('Invalid token');
+    }
   }
 }
